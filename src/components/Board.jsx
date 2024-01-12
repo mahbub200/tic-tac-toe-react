b0ad34c571f69136eafbd04ec565c6140faf165c
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-01-12 22:35:27 +0600

    adding interactivity

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,19 +5,19 @@ export default function Board() {
   return (
     <>
       <div className="flex">
-        <Square value="1"></Square>
-        <Square value="2"></Square>
-        <Square value="3"></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className="flex">
-        <Square value="4"></Square>
-        <Square value="5"></Square>
-        <Square value="6"></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className="flex">
-        <Square value="7"></Square>
-        <Square value="8"></Square>
-        <Square value="9"></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
     </>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Square = ({ value }) => {
+const Square = () => {
+  const [value, setValue] = useState(null);
+  function clickHandler() {
+    setValue("x");
+  }
   return (
     <>
-      <button className="bg-white border border-blue-400  m-1 h-24 w-24">
+      <button
+        className="bg-white border border-blue-400  m-1 h-24 w-24 "
+        onClick={clickHandler}
+      >
         {value}
       </button>
     </>
