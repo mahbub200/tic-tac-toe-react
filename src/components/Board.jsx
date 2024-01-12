9b254c43fca143d17fe46bda380d2c2d47c2f695
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-01-13 00:54:25 +0600

    passing props and setting values to squares

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,23 +3,31 @@ import Square from "./Square";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-
+  function clickHandler(i) {
+    // squares[0] = "X";
+    // setSquares([...squares]);
+    const nextSquares = squares.slice();
+    nextSquares[i] = "x";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]}></Square>
-        <Square value={squares[1]}> </Square>
-        <Square value={squares[2]}></Square>
+        <Square value={squares[0]} squareClick={() => clickHandler(0)}></Square>
+        <Square value={squares[1]} squareClick={() => clickHandler(1)}>
+          {" "}
+        </Square>
+        <Square value={squares[2]} squareClick={() => clickHandler(2)}></Square>
       </div>
       <div className="flex">
-        <Square value={squares[3]}></Square>
-        <Square value={squares[4]}></Square>
-        <Square value={squares[5]}></Square>
+        <Square value={squares[3]} squareClick={() => clickHandler(3)}></Square>
+        <Square value={squares[4]} squareClick={() => clickHandler(4)}></Square>
+        <Square value={squares[5]} squareClick={() => clickHandler(5)}></Square>
       </div>
       <div className="flex">
-        <Square value={squares[6]}></Square>
-        <Square value={squares[7]}></Square>
-        <Square value={squares[8]}></Square>
+        <Square value={squares[6]} squareClick={() => clickHandler(6)}></Square>
+        <Square value={squares[7]} squareClick={() => clickHandler(7)}></Square>
+        <Square value={squares[8]} squareClick={() => clickHandler(8)}></Square>
       </div>
     </>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Square = ({ value }) => {
+const Square = ({ value, squareClick }) => {
   return (
     <>
-      <button className="bg-white border border-blue-400  m-1 h-24 w-24 ">
+      <button
+        className="bg-white border border-blue-400  m-1 h-24 w-24 "
+        onClick={squareClick}
+      >
         {value}
       </button>
     </>
